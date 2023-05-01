4e57f9fc8f3de6ab02680b759588b039a8ef8253
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-01 10:43:46 +0800

    wm: Fix a possible race condition for `onRecentTaskRemoved`
    * notify tast stack changes after task/s was/were removed from recents
    
    attempt to fix: overview getting stucked (not dismissing view) after removing last task fron recents
    
    bug: 137386250
    Suggested by: Winson Chung <winsonc@google.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/com/android/internal/util/colt/PixelPropsUtils.java
+++ core/java/com/android/internal/util/colt/PixelPropsUtils.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.internal.util.rising;
+package com.android.internal.util.colt;
 
 import android.app.Application;
 import android.content.res.Resources;
